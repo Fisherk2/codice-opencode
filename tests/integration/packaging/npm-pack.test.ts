f41a90687d99da3316210d74f3f38018e74a9cbd
Fisherk2 <fisher_k2@hotmail.com>
2026-07-11 11:57:13 -0600

    chore: remove unused os import after replacing os.tmpdir() with process.cwd()
    Co-Authored-By: Huitzilopochtli <dev@fisherk2.com>

--- tests/integration/packaging/npm-pack.test.ts
+++ tests/integration/packaging/npm-pack.test.ts
@@ -10,7 +10,6 @@
 
 import { afterAll, beforeAll, describe, expect, it } from "bun:test";
 import * as fs from "node:fs/promises";
-import * as os from "node:os";
 import * as path from "node:path";
 import {
 	cleanupTempDir,
